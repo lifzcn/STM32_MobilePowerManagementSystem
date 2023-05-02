61f7d55e536c021d0a92c65d5be10010852749a1
LiFZ <lifzcn@qq.com>
2023-05-02 14:23:45 +0800

    Update some files

--- Project/Core/Src/main.c
+++ Project/Core/Src/main.c
@@ -26,7 +26,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "oled.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
--- Project/MDK-ARM/Project/oled.d
+++ Project/MDK-ARM/Project/oled.d
@@ -1,4 +1,4 @@
-project/main.o: ..\Core\Src\main.c ..\Core\Inc\main.h \
+project/oled.o: ..\Core\Src\oled.c ..\Core\Inc\oled.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal.h \
   ..\Core\Inc\stm32f1xx_hal_conf.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_rcc.h \
@@ -31,5 +31,4 @@ project/main.o: ..\Core\Src\main.c ..\Core\Inc\main.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_tim.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_tim_ex.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_uart.h \
-  ..\Core\Inc\adc.h ..\Core\Inc\i2c.h ..\Core\Inc\tim.h \
-  ..\Core\Inc\usart.h ..\Core\Inc\gpio.h ..\Core\Inc\oled.h
+  ..\Core\Inc\i2c.h ..\Core\Inc\main.h ..\Core\Inc\oledfont.h
